f023c02c75dcbc5487f69e8fa36f392299b515dc
Aaron James Lanot <aaronlanot@gmail.com>
2018-08-25 16:56:35 +0800

    redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,28 @@ var slideUp = {
     duration: 900
 };
 
+function addClassOnHover(element, className, targetElement = false) {
+    if(targetElement){
+        element.hover(function () {
+            targetElement.addClass(className);
+        },
+        function () {
+            targetElement.removeClass(className);
+        });
+    } else {
+        element.hover(function () {
+            element.addClass(className);
+        },
+        function () {
+            element.removeClass(className);
+        });
+    }
+
+}
+
 $(function(){
     ScrollReveal().reveal($('.reveal'), slideUp); 
+    addClassOnHover($('.one'),'show',$('.1-desc'));
+    addClassOnHover($('.two'),'show',$('.2-desc'));
+    addClassOnHover($('.three'),'show',$('.3-desc'));
 });
